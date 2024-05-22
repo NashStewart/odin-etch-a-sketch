323f26e26696d783818170690a0e72f3644e67cd
Nash Stewart <nashkstewart@gmail.com>
2024-05-22 08:01:19 -0600

    Add change grid button and pixel shading
    Add 10% opacity change everytime the mouse re-enters a pixel. Creating a
    shading effect. Add a button to prompt the user for a new grid size.
    Reset the grid according to the new size.

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 'use strct'
-console.log('Hello, World!');
 
-const gridContainer = document.querySelector('.grid-container');
+const baseContainer = document.querySelector('.base-container');
+const changePixelsButton = document.querySelector('.change-pixels');
 
-function renderGrid() {
-  for (let i = 0; i < 16; i++) {
-    const element = document.createElement('div');
-    element.classList.add('grid-item');
-    gridContainer.appendChild(element);
+let gridContainer = document.querySelector('.grid-container');
+
+function createGridItem() {
+  const item = document.createElement('div');
+  item.classList.add('grid-item');
+
+  item.addEventListener('mouseenter', () => {
+    item.classList.add('grid-item-colored');
+    const bgColor = window.getComputedStyle(item).backgroundColor;
+    let alpha = parseFloat(bgColor.slice(0, -1).split(',')[3]);
+    if (alpha < 1) {
+      const newAlpha = alpha + 0.1;
+      const newBgColor = bgColor.replace(alpha, newAlpha);
+      item.style.backgroundColor = newBgColor;
+    }
+  });
+
+  return item;
+}
+
+function renderGrid(size) {
+  if (isNaN(size) || size < 1) { size = 2 };
+  if (size > 100) { size = 100; };
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+    gridContainer.appendChild(row);
+
+    for (let j = 0; j < size; j++) {
+      const item = createGridItem();
+      row.appendChild(item);
+    }
   }
-};
+}
 
-renderGrid();
+changePixelsButton.addEventListener('click', () => {
+  const size = prompt('Input a size between 1-100.');
+
+  baseContainer.removeChild(gridContainer);
+  gridContainer = document.createElement('div');
+  gridContainer.classList.add('grid-container');
+  baseContainer.appendChild(gridContainer);
+
+  renderGrid(size);
+});
+
+renderGrid(50);
